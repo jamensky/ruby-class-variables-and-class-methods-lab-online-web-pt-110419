8d79789fbe95039e9f6dc8b6c6cde0b288ee8cea
Jamal Rjoul <jamensky1991@gmail.com>
2019-12-07 19:41:13 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @@count += 1
+    @name = name
+    @artist = artist
+    @@artists << @artist
+    @genre = genre
+    @@genres << @genre
+    binding.pry
+  end
+
+  def self.count
+    @@genres
+  end
+
+  def self.artists
+    return @@artists.uniq
+  end
+
+  def self.genres
+    return @@genres.uniq
+  end
+
+  def self.genre_count
+
+  end
+
+  def self.artist_count
+
+  end
+
+end
